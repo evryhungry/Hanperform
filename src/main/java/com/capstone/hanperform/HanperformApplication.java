1a5554f4d7826dc0e94455e1efecb23fe3c07a9e
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-01-20 20:26:35 +0900

    🎉 Start Initial project!!

---
+++ src/main/java/com/capstone/hanperform/HanperformApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.hanperform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanperformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanperformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/hanperform/HanperformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.hanperform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanperformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
