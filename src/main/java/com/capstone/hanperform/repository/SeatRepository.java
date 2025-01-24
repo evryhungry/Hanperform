0b41998df51b11dffcd8b55581cc2bd4254fce58
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-01-25 02:44:48 +0900

    Delete SeatRepository.java

--- src/main/java/com/capstone/hanperform/repository/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.hanperform.repository;
-
-import com.capstone.hanperform.entity.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-}
