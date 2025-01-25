08e08ca46a0d51009f8e19124ce603342475d2ac
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-01-25 14:19:14 +0900

    Delete log

--- src/main/java/com/capstone/hanperform/HomeController.java
+++ src/main/java/com/capstone/hanperform/HomeController.java
@@ -21,24 +21,19 @@ public class HomeController {
         this.performancesService = performancesService;
     }
 
-    // 공연 목록 페이지
     @GetMapping("/index")
     public String showIndex(Model model, HttpSession session) {
-        log.info("====> showIndex() 실행됨");
-        // 세션에서 로그인한 사용자 확인 (디버깅용 로그 추가)
+
         UserDto loggedInUser = (UserDto) session.getAttribute("loggedInUser");
-        log.info("로그인된 사용자: {}", loggedInUser);
 
         // 로그인한 사용자만 접근 가능하게 하려면 유지
         // if (loggedInUser == null) {
         //     return "redirect:/users/login";
         // }
 
-        // 공연 목록 불러오기 (디버깅용 로그 추가)
         List<PerformancesResponseDto> performances = performancesService.getAllPerformances();
         log.info("불러온 공연 목록 수: {}", performances.size());
 
-        // 모델에 공연 목록 추가
         model.addAttribute("performances", performances);
         return "index"; // index.html 렌더링
     }
